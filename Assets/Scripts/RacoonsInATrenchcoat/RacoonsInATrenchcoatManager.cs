cf8d040df3ad261efb18f68120e6e02733f2d033
Cirtoyt <conradjwilson@hotmail.com>
2026-01-30 21:53:23 +0000

    +Added RacoonFamily input actions, base RacoonsInATrenchcoat prefab, and individual Racoon prefabs, each with their own scripts, and a base manager script that has references to the other Racoon scripts.

---
+++ Assets/Scripts/RacoonsInATrenchcoat/RacoonsInATrenchcoatManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class RacoonsInATrenchcoatManager : MonoBehaviour
+{
+    [SerializeField] private LegRacoon _leftLegRacoon;
+    [SerializeField] private LegRacoon _rightLegRacoon;
+    [SerializeField] private ArmRacoon _leftArmRacoon;
+    [SerializeField] private ArmRacoon _rightArmRacoon;
+    [SerializeField] private HeadRacoon _headRacoon;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}

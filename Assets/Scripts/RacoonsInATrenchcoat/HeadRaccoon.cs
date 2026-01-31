0d41a4e3dbf36461956d8fa0a7968c2b775cf56b
Cirtoyt <conradjwilson@hotmail.com>
2026-01-31 14:36:26 +0000

    +Added HeadRaccoon look left/right/up/neutral smoothed rotation based on input combos.

--- Assets/Scripts/RacoonsInATrenchcoat/HeadRaccoon.cs
+++ Assets/Scripts/RacoonsInATrenchcoat/HeadRaccoon.cs
@@ -1,15 +1,124 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using static LegRaccoon;
 
 public class HeadRaccoon : MonoBehaviour
 {
-    private void OnHeadRaccoonAction1()
+    public enum HeadDirection
     {
-        Debug.Log("Test1");
+        Neutral,
+        Left,
+        Right,
+        Up,
     }
 
-    private void OnHeadRaccoonAction2()
+    [SerializeField] private Transform _neckPivot;
+    [SerializeField] private float _shakeHeadDegrees = 30;
+    [SerializeField] private float _nodHeadDegrees = 20;
+    [SerializeField] private float _neutralHeadDegrees = 10;
+    [SerializeField] private float _rotateHeadSpeed = 1;
+
+    private InputAction leftAction;
+    private InputAction rightAction;
+    private bool leftActionHeld = false;
+    private bool rightActionHeld = false;
+
+    private void Awake()
     {
-        Debug.Log("Test2");
+        leftAction = InputSystem.actions.FindAction("HeadRaccoonAction1");
+        rightAction = InputSystem.actions.FindAction("HeadRaccoonAction2");
+    }
+
+    private void OnEnable()
+    {
+        leftAction.started += OnLeftActionStarted;
+        rightAction.started += OnRightActionStarted;
+        leftAction.canceled += OnLeftActionCanceled;
+        rightAction.canceled += OnRightActionCanceled;
+    }
+
+    private void OnDisable()
+    {
+        leftAction.started -= OnLeftActionStarted;
+        rightAction.started -= OnRightActionStarted;
+        leftAction.canceled -= OnLeftActionCanceled;
+        rightAction.canceled -= OnRightActionCanceled;
+    }
+
+    private void OnLeftActionStarted(InputAction.CallbackContext obj)
+    {
+        leftActionHeld = true;
+    }
+
+    private void OnRightActionStarted(InputAction.CallbackContext obj)
+    {
+        rightActionHeld = true;
+    }
+
+    private void OnLeftActionCanceled(InputAction.CallbackContext obj)
+    {
+        leftActionHeld = false;
+    }
+
+    private void OnRightActionCanceled(InputAction.CallbackContext obj)
+    {
+        rightActionHeld = false;
+    }
+
+    public HeadDirection GetTargetHeadDirection()
+    {
+        if (!leftActionHeld && !rightActionHeld)
+        {
+            Debug.Log("Head target direction set to neutral");
+            return HeadDirection.Neutral;
+        }
+        else if (leftActionHeld && !rightActionHeld)
+        {
+            Debug.Log("Head target direction set to left");
+            return HeadDirection.Left;
+        }
+        else if (rightActionHeld && !leftActionHeld)
+        {
+            Debug.Log("Head target direction set to right");
+            return HeadDirection.Right;
+        }
+        else// if (leftActionHeld && rightActionHeld)
+        {
+            Debug.Log("Head target direction set to up");
+            return HeadDirection.Up;
+        }
+    }
+
+    public void GetCurrentLookDirection(out Vector3 localForwardDirection, out Quaternion localRotation)
+    {
+        localForwardDirection = Vector3.zero;
+        localRotation = Quaternion.identity;
+
+        switch (GetTargetHeadDirection())
+        {
+            case HeadDirection.Neutral:
+                localRotation = Quaternion.Euler(_neutralHeadDegrees, 0, 0);
+                break;
+            case HeadDirection.Left:
+                localRotation = Quaternion.Euler(0, -_shakeHeadDegrees, 0);
+                break;
+            case HeadDirection.Right:
+                localRotation = Quaternion.Euler(0, _shakeHeadDegrees, 0);
+                break;
+            case HeadDirection.Up:
+                localRotation = Quaternion.Euler(-_nodHeadDegrees, 0, 0);
+                break;
+        }
+
+        localForwardDirection = localRotation * localForwardDirection;
+    }
+
+    private void FixedUpdate()
+    {
+        // Smooth move head to facing target direction
+
+        GetCurrentLookDirection(out Vector3 targetLocalForward, out Quaternion targetLocalRotation);
+
+        _neckPivot.localRotation = Quaternion.Slerp(_neckPivot.localRotation, targetLocalRotation, _rotateHeadSpeed * Time.deltaTime);
     }
 }

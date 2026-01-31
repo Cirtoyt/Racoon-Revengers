a4e03bb2859104a6dac441d707c44df05ef9201b
Rosa <rosakay4@gmail.com>
2026-01-31 11:56:00 +0000

    basic estate agent logic
    waypoints, dialogue

---
+++ Assets/Scripts/Estate Agent/TaskAssignment.cs
@@ -0,0 +1,54 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class TaskAssignment : MonoBehaviour
+{
+    //Dialogue
+    [SerializeField] private TextMeshProUGUI subtitles;
+    [SerializeField] private List<string> dialogue;
+    private int currentIndex;
+
+    //Movement
+    private NavMeshAgent agent;
+    [SerializeField] private List<GameObject> waypoints;
+    private int currentWaypoint;
+
+    //Canvas
+    [SerializeField] private TextMeshProUGUI task;
+    [SerializeField] private RawImage textBox;
+    [SerializeField] private Animator textboxAnim;
+    
+
+    private void Start()
+    {
+        subtitles.text = dialogue[0];
+        currentIndex = 0;
+        currentWaypoint = 0;
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+    public void StartDialogue()
+    {
+        subtitles.text = dialogue[0];
+    }
+
+    public void AdvanceText()
+    {
+        subtitles.text = dialogue[currentIndex + 1];
+        currentIndex++;
+    }
+
+    public void NextWaypoint()
+    {
+        agent.SetDestination(waypoints[currentWaypoint + 1].transform.position);
+        currentWaypoint++;
+    }
+
+
+}
---
+++ Assets/Scripts/Testing Scripts/TEST movement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TESTmovement : MonoBehaviour
+{
+    private int speed = 5;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

b3802deb7a3455026f22eacbe9647ea5d4ddbfa9
Evey <taunull47@gmail.com>
2026-02-01 16:34:11 +0000

    Changed reset button

--- Assets/Scripts/Systems/GameController.cs
+++ Assets/Scripts/Systems/GameController.cs
@@ -23,7 +23,7 @@ public class GameController : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R))
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
             fadeOut.StartFade(fadeOutDuration);
         }

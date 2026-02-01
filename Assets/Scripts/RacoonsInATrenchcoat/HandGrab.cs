7570660dff98a7052f20c98f5cea447f93c71181
Evey <taunull47@gmail.com>
2026-02-01 11:37:17 +0000

    Grab and throw objects

--- Assets/Scripts/RacoonsInATrenchcoat/HandGrab.cs
+++ Assets/Scripts/RacoonsInATrenchcoat/HandGrab.cs
@@ -1,13 +1,38 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HandGrab : MonoBehaviour
 {
+    private ArmRaccoon armRaccoon;
+
     private bool grabbing = false;
-    private GameObject heldObject = null;
+    private Grabbable heldObject = null;
     private Rigidbody objectRigidbody = null;
 
-    public void Grab(GameObject other)
+    private void Awake()
     {
+        Transform parent = transform.parent;
+        while(parent != null)
+        {
+            armRaccoon = parent.GetComponent<ArmRaccoon>();
+            if (armRaccoon != null)
+            {
+                break;
+            }
+            parent = parent.parent;
+        }
+        if(armRaccoon == null)
+        {
+            Debug.LogError("No arm raccoon parent found!");
+        }
+    }
+
+    public void Grab(Grabbable other, Vector3 collisionPoint)
+    {
+        if(other == null)
+        {
+            return;
+        }
         if (!grabbing)
         {
             return;
@@ -16,16 +41,15 @@ public class HandGrab : MonoBehaviour
         {
             return;
         }
-        if(other.GetComponent<Grabbable>())
+        heldObject = other;
+        other.transform.parent = this.transform;
+        //other.transform.position = collisionPoint;
+        other.gameObject.layer = 3;
+        objectRigidbody = other.GetComponent<Rigidbody>();
+        if(objectRigidbody)
         {
-            heldObject = other;
-            other.transform.parent = this.transform;
-            objectRigidbody = other.GetComponent<Rigidbody>();
-            if(objectRigidbody)
-            {
-                objectRigidbody.useGravity = false;
-                objectRigidbody.isKinematic = true;
-            }
+            objectRigidbody.useGravity = false;
+            objectRigidbody.isKinematic = true;
         }
     }
 
@@ -35,11 +59,17 @@ public class HandGrab : MonoBehaviour
 
         if (heldObject && !grabbing)
         {
+            heldObject.gameObject.layer = heldObject.layer;
             heldObject.transform.parent = null;
             if (objectRigidbody)
             {
                 objectRigidbody.useGravity = true;
                 objectRigidbody.isKinematic = false;
+            }
+            Rigidbody rigidbody = heldObject.GetComponent<Rigidbody>();
+            if (rigidbody)
+            {
+                rigidbody.AddForceAtPosition(armRaccoon.transform.forward * 2.0f, rigidbody.transform.position + Vector3.up, ForceMode.Impulse);
             }
             heldObject = null;
             objectRigidbody = null;

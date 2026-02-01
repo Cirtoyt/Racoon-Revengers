a6010542554bc5b2b58d8f5e2fb33ff185fe6285
Cirtoyt <conradjwilson@hotmail.com>
2026-02-01 11:43:37 +0000

    +Finished adding base leg movement that pivots along with head facing direction & added a delay timer to prevent stepping inputs processing rapidly/spammable.

--- Assets/Scripts/RacoonsInATrenchcoat/RaccoonsInATrenchcoatManager.cs
+++ Assets/Scripts/RacoonsInATrenchcoat/RaccoonsInATrenchcoatManager.cs
@@ -5,17 +5,23 @@ public class RaccoonsInATrenchcoatManager : MonoBehaviour
     static public RaccoonsInATrenchcoatManager Instance => instance;
     static private RaccoonsInATrenchcoatManager instance;
 
+    [Header("Statics")]
     [SerializeField] private LegRaccoon _leftLegRaccoon;
     [SerializeField] private LegRaccoon _rightLegRaccoon;
     [SerializeField] private ArmRaccoon _leftArmRaccoon;
     [SerializeField] private ArmRaccoon _rightArmRaccoon;
     [SerializeField] private HeadRaccoon _headRaccoon;
+    [Header("Properties")]
+    [SerializeField] private float _stepDelay = 0.5f;
 
     public LegRaccoon LeftLegRaccoon => _leftLegRaccoon;
     public LegRaccoon RightLegRaccoon => _rightLegRaccoon;
     public ArmRaccoon LeftArmRaccoon => _leftArmRaccoon;
     public ArmRaccoon RightArmRaccoon => _rightArmRaccoon;
     public HeadRaccoon HeadRaccoon => _headRaccoon;
+
+    private bool stepDelayActive = false;
+    private float stepDelayTimer = 0;
 
     private void Awake()
     {
@@ -33,6 +39,25 @@ public class RaccoonsInATrenchcoatManager : MonoBehaviour
 
     private void Update()
     {
-        
+        if (stepDelayActive)
+        {
+            stepDelayTimer += Time.deltaTime;
+            if (stepDelayTimer >= _stepDelay)
+            {
+                // Reset
+                stepDelayTimer = 0;
+                stepDelayActive = false;
+            }
+        }
+    }
+
+    public void BeginStepDelay()
+    {
+        stepDelayActive = true;
+    }
+
+    public bool IsStepDelayUp()
+    {
+        return !stepDelayActive;
     }
 }
